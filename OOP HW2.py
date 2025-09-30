49ab373828a96f13e2ad6aefa9767814e5b8a567
Evgeny <fenrir_lokisonn@mail.ru>
2025-09-30 11:43:50 +0300

    task 3 new

--- OOP HW2.py
+++ OOP HW2.py
@@ -1,70 +1,60 @@
-# TASK 1
-
-cook_book = {}
-with open('recipes.txt', 'rt', encoding='utf-8') as file:
-    dishes = ''
-    for x in file:
-        x = x.strip()
-        if x.isdigit():
-            continue
-        elif x and '|' not in x:
-            cook_book[x] = []
-            dishes = x
-        elif x and '|' in x:
-            a, b, c = x.split(" | ")
-            cook_book.get(dishes).append(dict(ingredient_name=a, quantity=int(b), measure=c))
-# print(cook_book)
-
-# Задача №2:
-def get_shop_list_by_dishes(dishes_list, person_count):
-    shop_list = {}
-    for dish in dishes_list:
-        if dish in cook_book:
-            for ingredient in cook_book[dish]:
-                if ingredient['ingredient_name'] in shop_list:
-                    shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
-                else:
-                    shop_list[ingredient['ingredient_name']] = ({'measure': ingredient['measure'], 'quantity':
-                                                                (ingredient['quantity'] * person_count)})
-        else:
-            print('Такого блюда нет в книге')
-    return shop_list
-# print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+# # TASK 1
+#
+# cook_book = {}
+# with open('recipes.txt', 'rt', encoding='utf-8') as file:
+#     dishes = ''
+#     for x in file:
+#         x = x.strip()
+#         if x.isdigit():
+#             continue
+#         elif x and '|' not in x:
+#             cook_book[x] = []
+#             dishes = x
+#         elif x and '|' in x:
+#             a, b, c = x.split(" | ")
+#             cook_book.get(dishes).append(dict(ingredient_name=a, quantity=int(b), measure=c))
+# # print(cook_book)
+#
+# # Задача №2:
+# def get_shop_list_by_dishes(dishes_list, person_count):
+#     shop_list = {}
+#     for dish in dishes_list:
+#         if dish in cook_book:
+#             for ingredient in cook_book[dish]:
+#                 if ingredient['ingredient_name'] in shop_list:
+#                     shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
+#                 else:
+#                     shop_list[ingredient['ingredient_name']] = ({'measure': ingredient['measure'], 'quantity':
+#                                                                 (ingredient['quantity'] * person_count)})
+#         else:
+#             print('Такого блюда нет в книге')
+#     return shop_list
+# # print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
 
 
 # Задача №3:
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    line_1 = {}
-    count_1 = 0
-    for line in file_1.readlines():
-        count_1 += 1
-        line_1['1.txt'] = count_1
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    text_1 = file_1.read()
+import os
 
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    line_2 = {}
-    count_2 = 0
-    for line in file_2.readlines():
-        count_2 += 1
-        line_2['2.txt'] = count_2
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    text_2 = file_2.read()
 
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    line_3 = {}
-    count_3 = 0
-    for line in file_3.readlines():
-        count_3 += 1
-        line_3['3.txt'] = count_3
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    text_3 = file_3.read()
+cwd = os.getcwd()
+dir_with_files = 'C:/NETO/cook_book/files_for_task_3'
 
-join = sorted(list(line_1.items()) + list(line_2.items()) + list(line_3.items()), key=lambda x: x[1])
+# Список имён файлов .txt из указанной дирректории
+txt_files = [f for f in os.listdir(dir_with_files) if os.path.isfile(os.path.join(dir_with_files, f)) and f.endswith('.txt')]
 
-with open('result.txt', 'w', encoding='utf-8') as file_result:
-    for line in join:
-        file_result.write(f'{join[0][0]}\n {join[0][1]}\n {text_2}\n {join[1][0]}\n {join[1][1]}\n {text_1}\n'
-                          f'{join[2][0]}\n {join[2][1]}\n {text_3}\n')
+# Словарь с именем и кол-вом строк файла
+files_info_dict = {}
+for txt_file in txt_files:
+    with open(os.path.join(cwd, dir_with_files, txt_file), encoding='utf-8') as f:
+        lines = f.readlines()
+        files_info_dict[txt_file] = (len(lines), lines)
 
-print(file_result)
+# Сортируем в порядке возрастания кол-ва строк
+files_info_tuple = sorted(files_info_dict.items(), key=lambda item: item[1])
+
+# Запись результата в файл
+with open('C:/NETO/cook_book/result.txt', 'w', encoding='utf-8') as result:
+    for file_info in files_info_tuple:
+        result.write(f'{file_info[0]}\n')
+        result.write(f'{file_info[1][0]}\n')
+        result.writelines(file_info[1][1] + ['\n'])
